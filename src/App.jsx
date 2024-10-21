dd3c8947b4926af68b1202318997fc9fdcd68461
Soyeon <isylsy166@gmail.com>
2024-10-21 21:00:06 +0900

    초기 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { createGlobalStyle } from 'styled-components';
-import './App.css';
-import Router from './Router';
+import './css/App.css';
 
-export const theme = {
-    bgColor : '#ffeae4',
-    accentColor: '#f9724a'
-}
-
-const GlobalStyle = createGlobalStyle`
-
-  body{
-    background-color: ${theme.bgColor};
-    transition: all 1s ease;
-  }
-
-`
 
 function App() {
 
   return (
     <>
-      <GlobalStyle/>
-      <Router/>
+
     </>
   );
 }
