f54cd3ff4039678cf8a0aa8382f54abdb2978d3a
Soyeon <isylsy166@gmail.com>
2024-10-26 04:35:55 +0900

    라우터 못하고 UI만 만지작 (#9)

---
+++ src/styles/emotion/followMouse.style.js
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const sizeUp = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(2.5);
+  }
+`;
+
+const sizeDown = keyframes`
+  0% {
+    transform: scale(2.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+export const Cursor = styled.div`
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    pointer-events: none; /* 커서 요소와의 상호작용 방지 */
+    background-color: #ffc0cbc6;
+    animation: ${props => props.isHover ? sizeUp : sizeDown} 0.5s ease-in-out forwards;
+
+`
