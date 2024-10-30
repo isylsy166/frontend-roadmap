f3f2ee7f03e2fc4fb6e2691d4367f90a27a53ecf
Soyeon <isylsy166@gmail.com>
2024-10-30 14:19:30 +0900

    로드맵 페이지

--- src/pages/roadmapPage/pc/roadmap.jsx
+++ src/pages/roadmapPage/pc/roadmap.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from '../../../styles/css/page/roadmap.module.css'
+import RoadmapTitles from './titles/roadmapTitles'
+import LayoutContent from '../../layout/pc/layoutContent'
 
 export default function Roadmap() {
+
+  const [showModal, setShowModal] = useState(false);
+  console.log(showModal);
+
   return (
-    <div>로드맵</div>
+    <div className={style.frame}>
+      <RoadmapTitles setShowModal={setShowModal}/>
+
+      <LayoutContent showModal={showModal} setShowModal={setShowModal}>
+
+      </LayoutContent>
+    </div>
   )
 }
